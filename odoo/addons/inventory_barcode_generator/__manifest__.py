9d77c71f54ec436e0920ea256c5788c082564d95
adityanugraha23 <aditya@hungryhub.com>
2024-07-04 21:44:56 +0700

    fix errors

--- odoo/addons/inventory_barcode_generator/__manifest__.py
+++ odoo/addons/inventory_barcode_generator/__manifest__.py
@@ -7,7 +7,7 @@
         This module allows users to generate barcodes for products in the inventory.
     """,
     'author': 'Aditya',
-    'depends': ['base', 'product'],
+    'depends': ['base','stock','product'],
     'data': [
         'views/product_template_views.xml',
         'security/barcode_inventory_move_security.xml',
@@ -16,8 +16,10 @@
         'views/barcode_inventory_move_wizard_view.xml',
         'views/barcode_preview_wizard_view.xml', 
         'views/inventory_views.xml',
-        'views/barcode_inventory_move_view.xml',
-      
+         'views/barcode_inventory_move_view.xml',
+        'views/barcode_inventory_move_menu.xml',
+       
+       
     ],
     'installable': True,
     'application': False,
